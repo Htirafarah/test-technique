6819dc9dfb4fdf59a5192586d0482ebd56b2a10f
Farah HTIRA <htirafarah@gmail.com>
2025-01-14 21:55:21 +0100

    Add files via upload

---
+++ etal.py
@@ -0,0 +1,20 @@
+def Somme_etalonnage(document):
+    try:
+        with open(document, 'r') as document:
+            lignes = document.readlines()
+        somme_finale = 0
+        for ligne in lignes:
+            ligne = ligne.strip()
+            chiffres = [char for char in ligne if char.isdigit()]
+            valeur_etalonnage = int(chiffres[0] + chiffres[-1])
+            somme_finale += valeur_etalonnage
+        return somme_finale
+
+    except FileNotFoundError:
+        print("Fichier introuvable")
+        return None
+
+nom_fichier = "document.txt"
+resultat = Somme_etalonnage(nom_fichier)
+if resultat is not None:
+    print(f"La somme des valeurs d'étalonnage est: {resultat}")
